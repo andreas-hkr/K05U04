1c87fddad51f414c31b1ee316007dcf980b0b3cf
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-15 17:51:22 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Läs in en text och skriv ut den baklänges
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ange en text: ");
+        String text = input.nextLine();
 
+        for (int i=text.length()-1; i >= 0; i--) {
+            System.out.print(text.charAt(i));
+        }
     }
 }
